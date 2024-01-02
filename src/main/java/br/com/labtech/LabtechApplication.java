4a7e7d78f265c5e28759bcc9bee5c9518f4a079e
Taui Silva <tauisilva@gmail.com>
2024-01-02 20:48:31 -0300

    Update LabtechApplication.java

--- src/main/java/br/com/labtech/LabtechApplication.java
+++ src/main/java/br/com/labtech/LabtechApplication.java
@@ -1,17 +1,26 @@
 package br.com.labtech;
 
+import br.com.labtech.utils.UrlConstant;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title="LabTech", version = "1", description = "Api Labtech"))
+@OpenAPIDefinition(info = @Info(title = "LabTech", version = "1", description = "Api Labtech"))
 public class LabtechApplication {
+  @Value("${api.url.photo}")
+  private String absolutePath;
 
-	public static void main(String[] args) {
-		SpringApplication.run(LabtechApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LabtechApplication.class, args);
+  }
+
+  @PostConstruct
+  public void init() {
+    UrlConstant.setAbsoluteUrl(this.absolutePath);
+  }
 
 }
