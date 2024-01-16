ccdd5c3e5fa3d154fed872e1e5379f96db7a31f8
Taui Silva <tauisilva@gmail.com>
2024-01-16 20:49:33 -0300

    Criação objeto ODS

---
+++ src/main/java/br/com/labtech/ods/ObjDesenSus.java
@@ -0,0 +1,27 @@
+package br.com.labtech.ods;
+
+import br.com.labtech.constants.SchemaConstants;
+import br.com.labtech.utils.AbstractEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Ods_tb", schema = SchemaConstants.EVENTOS)
+public class ObjDesenSus extends AbstractEntity {
+//  Objetivo de Desenvolvimento Sustentável
+
+  private String name;
+
+  private String description;
+
+  private boolean status;
+
+}
---
+++ src/main/java/br/com/labtech/ods/ObjDesenSusDTO.java
@@ -0,0 +1,19 @@
+package br.com.labtech.ods;
+
+import br.com.labtech.utils.AbstractEntityDTO;
+import lombok.*;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class ObjDesenSusDTO extends AbstractEntityDTO {
+
+  private String name;
+
+  private String description;
+
+  private boolean status;
+
+}
